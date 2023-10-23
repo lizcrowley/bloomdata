bfbfa156ba9bcb31815d41ac2db204c876f1a2a5
lizcrowley <lizcrowley12@gmail.com>
2023-10-22 22:45:23 -0500

    PEP8 corrections

--- bloomdata/acme.py
+++ bloomdata/acme.py
@@ -1,36 +1,40 @@
+# pylint: disable=too-many-arguments
 '''This class is meant to create products with certain variables for
 a practice example of working at the Acme Corporation.'''
 import random
 
-'''Product class creates product information and determines certain
-factors of given product.'''
+
 class Product:
 
-    '''Initialize Product class with given variables: name is name of the 
+    '''Initialize Product class with given variables: name is name of the
     product, identifier is a random integer between 1000000 and 9999999, price
-    is defaulted at 10, weight is defaulted at 20, and flammability is 
+    is defaulted at 10, weight is defaulted at 20, and flammability is
     defaulted at 0.5.'''
-    def __init__(self, name, identifier=random.randint(1000000, 9999999), 
-                 price=10, weight=20, flammability=0.5):
+    def __init__(self,
+                 name,
+                 identifier=random.randint(1000000, 9999999),
+                 price=10,
+                 weight=20,
+                 flammability=0.5):
         self.name = name
         self.identifier = identifier
         self.price = price
         self.weight = weight
         self.flammability = flammability
 
-    '''Stealability method calculates the price devided by the weight and returns
-    string based on how likely product will be to steal.'''
     def stealability(self):
+        '''Stealability method calculates the price devided by the weight and
+        returns string based on how likely product will be to steal.'''
         calc = self.price / self.weight
         if calc < 0.5:
             return "Not so stealable..."
         if calc < 1.0 and (calc > 0.5 or calc == 0.5):
             return "Kinda stealable."
         return "Very stealable!"
-    
-    '''Explode method calculates flammability multiplied by weight and returns
-    string based on how likely the product is to explode.'''
+
     def explode(self):
+        '''Explode method calculates flammability multiplied by weight and
+        returns string based on how likely the product is to explode.'''
         expl = self.flammability * self.weight
         if expl < 10:
             return "...fizzle."
@@ -39,27 +43,31 @@ class Product:
         return "...BABOOM!!"
 
     def __repr__(self):
-        prod_list = self.name, self.identifier, self.price, self.weight, self.flammability
+        prod_list = (self.name,
+                     self.identifier,
+                     self.price,
+                     self.weight,
+                     self.flammability)
         return f'{prod_list}'
-        #return f'''{self.name} has the identifier {self.identifier} with a price of {self.price}, 
-                    #a weight of {self.weight}, and a flammability value of {self.flammability}.'''
+        # return f'''{self.name} has the identifier {self.identifier} with a
+        #           price of {self.price}, a weight of {self.weight}, and a
+        #           flammability value of {self.flammability}.'''
 
-'''BoxingGlove is a class that inherits variables from the Product class but includes
-a few new methods for the child class.'''
+
 class BoxingGlove(Product):
 
-    '''Initializing the BoxingGlove class variables the same as the Product class but weight is
-    defaulted at 10 instead of 20.'''
-    def __init__(self, name, identifier=random.randint(1000000, 9999999)
-                 , price=10, weight=10, flammability=0.5):
+    '''Initializing the BoxingGlove class variables the same as the Product
+    class but weight is defaulted at 10 instead of 20.'''
+    def __init__(self, name, identifier=random.randint(1000000, 9999999),
+                 price=10, weight=10, flammability=0.5):
         super().__init__(name, identifier, price, weight, flammability)
 
-    '''Explode method now only returns the phrase "...it's a glove."'''
     def explode(self):
+        '''Explode method now only returns the phrase "...it's a glove."'''
         return "...it's a glove."
-    
-    '''Punch method returns a string based on what the weight of the BoxingGlove is.'''
+
     def punch(self):
+        '''Punch method returns string based on weight.'''
         if self.weight < 5:
             return "That tickles."
         if self.weight < 15 and (self.weight > 5 or self.weight == 5):
--- bloomdata/acme_test.py
+++ bloomdata/acme_test.py
@@ -3,26 +3,38 @@ from acme import Product
 from acme_report import generate_products, ADJECTIVES, NOUNS
 import pytest
 
-'''Testing to ensure defualt price is 10'''
+
+@pytest.fixture
+def prod_init():
+    '''pytest fixture to clean up code'''
+    return Product('Test Product')
+
+
 def test_default_product_price():
-    '''Test default price being 10.'''
-    prod = Product('Test Product')
+    '''Testing to ensure defualt price is 10'''
+    prod = prod_init()
     assert prod.price == 10
 
-'''Testing to ensure stealability() results in a str'''
+
 def test_stealability():
-    assert type(Product('Test Product').stealability()) == str
+    '''Testing to ensure stealability() results in a str'''
+    assert isinstance(prod_init().stealability(), str)
 
-'''Testing to ensure explode() results in a str'''
+
 def test_explode():
-    assert type(Product('Test Product').explode()) == str
+    '''Testing to ensure explode() results in a str'''
+    assert isinstance(prod_init().explode(), str)
 
-'''Testing to ensure Product().price is an int'''
+
 def test_attr():
-    price = Product('Test Product').price
-    assert type(Product('Test Product').price) == int
+    '''Testing to ensure Product().price is an int'''
+    price = prod_init().price
+    assert isinstance(price, int)
 
-'''Testing to ensure default list length is 30'''
+
 def test_gen_prod():
+    '''Testing to ensure default list length is 30'''
     prod = generate_products()
     assert len(prod) == 30
+    assert isinstance(ADJECTIVES[0], str)
+    assert isinstance(NOUNS[0], str)
